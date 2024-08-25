5e59fad2ab071f6d7c413cf4aeb34e5a869620f7
Zoom75x <Zoom-75x@yandex.ru>
2024-08-25 18:58:20 +0900

    миграция на реакт контекст добаволение ТДЛ

--- src/todolists/addTodolist/AddTodolist.tsx
+++ src/todolists/addTodolist/AddTodolist.tsx
@@ -1,29 +1,17 @@
-import { v4 as uuidv4 } from 'uuid'
 import { useContext, useState } from 'react'
 import { BaseButton, BaseInput } from '../../shared'
-import { TodoListType } from '../../types'
 import { TodolistContext } from '../../app/provider'
 
 export const AddTodolist = () => {
   const [value, setValue] = useState<string>('')
-
-  const { setTodolists, setTasksObj: setTasks } = useContext(TodolistContext)
-
-  const onClickAddTodolist = () => {
-    const todolistId = uuidv4()
-    const newTodolist: TodoListType = {
-      id: todolistId,
-      title: value,
-    }
-    const newTask = {
-      [todolistId]: [],
-    }
-    setTodolists((prevState) => [newTodolist, ...prevState])
-    setTasks((prevState) => {
-      return { ...prevState, ...newTask }
-    })
-    setValue('')
+  const { addTodolist } = useContext(TodolistContext)
+  const onClear =() => {
+    setValue("")
   }
+  const onClick =() => {
+    addTodolist(value, onClear)
+  }
+
   return (
     <div>
       <BaseInput
@@ -33,7 +21,7 @@ export const AddTodolist = () => {
         }}
         value={value}
       />
-      <BaseButton variant={'secondary'} onClick={onClickAddTodolist}>
+      <BaseButton variant={'secondary'} onClick={onClick}>
         Add Todo
       </BaseButton>
     </div>
